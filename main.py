50bf6b92da1b3985697021f5ea70db15c2e3803d
Rafal Stapinski <stapinskirafal@gmail.com>
2018-03-29 19:12:06 -0400

    copied stable marriage code

--- main.py
+++ main.py
@@ -1,9 +1,90 @@
-from PIL import Image
+# from PIL import Image
+# import math
+#
+# def distance_3d(a, b):
+#
+#     return math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2 + (b[2] - a[2]) ** 2)
+#
+# img = Image.open('imgs/ponies.jpg')
+#
+# reduced_img = img.convert('P', palette = Image.ADAPTIVE, colors = 64)
+#
+# colors = reduced_img.convert('RGB').getcolors()
+#
+# print colors
+#
+# print distance_3d((1, 1, 1), (0, 0, 0))
 
-img = Image.open('imgs/ponies.jpg')
 
-reduced_img = img.convert('P', palette = Image.ADAPTIVE, colors = 64)
+# set_a = {'a': (0, 0), 'b': (0, 1), 'c': (1, 1), 'd': (1, 0)}
+# set_b = {'w': (3, 3), 'x': (3, 4), 'y': (4, 4), 'z': (4, 3)}
+#
+#
 
-colors = reduced_img.convert('RGB').getcolors()
+import copy
 
-print colors
+guyprefers = {
+ 'abe':  ['abi', 'eve', 'cath', 'ivy', 'jan', 'dee', 'fay', 'bea', 'hope', 'gay'],
+ 'bob':  ['cath', 'hope', 'abi', 'dee', 'eve', 'fay', 'bea', 'jan', 'ivy', 'gay'],
+ 'col':  ['hope', 'eve', 'abi', 'dee', 'bea', 'fay', 'ivy', 'gay', 'cath', 'jan'],
+ 'dan':  ['ivy', 'fay', 'dee', 'gay', 'hope', 'eve', 'jan', 'bea', 'cath', 'abi'],
+ 'ed':   ['jan', 'dee', 'bea', 'cath', 'fay', 'eve', 'abi', 'ivy', 'hope', 'gay'],
+ 'fred': ['bea', 'abi', 'dee', 'gay', 'eve', 'ivy', 'cath', 'jan', 'hope', 'fay'],
+ 'gav':  ['gay', 'eve', 'ivy', 'bea', 'cath', 'abi', 'dee', 'hope', 'jan', 'fay'],
+ 'hal':  ['abi', 'eve', 'hope', 'fay', 'ivy', 'cath', 'jan', 'bea', 'gay', 'dee'],
+ 'ian':  ['hope', 'cath', 'dee', 'gay', 'bea', 'abi', 'fay', 'ivy', 'jan', 'eve'],
+ 'jon':  ['abi', 'fay', 'jan', 'gay', 'eve', 'bea', 'dee', 'cath', 'ivy', 'hope']}
+galprefers = {
+ 'abi':  ['bob', 'fred', 'jon', 'gav', 'ian', 'abe', 'dan', 'ed', 'col', 'hal'],
+ 'bea':  ['bob', 'abe', 'col', 'fred', 'gav', 'dan', 'ian', 'ed', 'jon', 'hal'],
+ 'cath': ['fred', 'bob', 'ed', 'gav', 'hal', 'col', 'ian', 'abe', 'dan', 'jon'],
+ 'dee':  ['fred', 'jon', 'col', 'abe', 'ian', 'hal', 'gav', 'dan', 'bob', 'ed'],
+ 'eve':  ['jon', 'hal', 'fred', 'dan', 'abe', 'gav', 'col', 'ed', 'ian', 'bob'],
+ 'fay':  ['bob', 'abe', 'ed', 'ian', 'jon', 'dan', 'fred', 'gav', 'col', 'hal'],
+ 'gay':  ['jon', 'gav', 'hal', 'fred', 'bob', 'abe', 'col', 'ed', 'dan', 'ian'],
+ 'hope': ['gav', 'jon', 'bob', 'abe', 'ian', 'dan', 'hal', 'ed', 'col', 'fred'],
+ 'ivy':  ['ian', 'col', 'hal', 'gav', 'fred', 'bob', 'abe', 'ed', 'jon', 'dan'],
+ 'jan':  ['ed', 'hal', 'gav', 'abe', 'bob', 'jon', 'col', 'ian', 'fred', 'dan']}
+
+def matchmaker(set_a_prefs, set_b_prefs):
+
+    guys = sorted(set_a_prefs.keys())
+    gals = sorted(set_b_prefs.keys())
+
+    guysfree = guys[:]
+    engaged  = {}
+    guyprefers2 = set_a_prefs
+    galprefers2 = set_b_prefs
+    while guysfree:
+        guy = guysfree.pop(0)
+        guyslist = guyprefers2[guy]
+        gal = guyslist.pop(0)
+        fiance = engaged.get(gal)
+        if not fiance:
+            # She's free
+            engaged[gal] = guy
+            print("  %s and %s" % (guy, gal))
+        else:
+            # The bounder proposes to an engaged lass!
+            galslist = galprefers2[gal]
+            if galslist.index(fiance) > galslist.index(guy):
+                # She prefers new guy
+                engaged[gal] = guy
+                print("  %s dumped %s for %s" % (gal, fiance, guy))
+                if guyprefers2[fiance]:
+                    # Ex has more girls to try
+                    guysfree.append(fiance)
+            else:
+                # She is faithful to old fiance
+                if guyslist:
+                    # Look again
+                    guysfree.append(guy)
+    return engaged
+
+
+print('\nEngagements:')
+engaged = matchmaker(guyprefers, galprefers)
+
+print('\nCouples:')
+print('  ' + ',\n  '.join('%s is engaged to %s' % couple
+                          for couple in sorted(engaged.items())))
